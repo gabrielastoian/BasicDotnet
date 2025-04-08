d52ca1985b01a73772eb2692589865e8729e2f19
gabrielastoian <gabrielastoian90@gmail.com>
2025-04-08 09:14:47 +0300

    commited program that calculates the are of a triangle

---
+++ AreaOfTriangle/Program.cs
@@ -0,0 +1,19 @@
+﻿//program that calculates the area of a triangle given the base and height
+
+//prompt the user for the base and height of the triangle
+Console.WriteLine("What is the base of the triangle?");
+var baseLength = Console.ReadLine();
+Console.WriteLine("What is the height of the triangle?");
+var height = Console.ReadLine(); 
+
+//parse the input to a double
+double baseLengthDouble = double.Parse(baseLength);
+double heightDouble = double.Parse(height);
+
+//calculate the area of the triangle
+double areaOfTriangle = (baseLengthDouble * heightDouble) / 2;
+
+//print the area of the triangle
+Console.WriteLine("The area of the triangle is " + areaOfTriangle);
+
+                                                                           
