70cdc33460b7e36269d539236af9bd4f3264b732
gabrielastoian <gabrielastoian90@gmail.com>
2025-04-21 20:57:44 +0300

    added Classes exercise

---
+++ Classes/Program.cs
@@ -0,0 +1,9 @@
+﻿//here goes the main method
+class Score {
+    public string name;
+    public int points;
+    public int level;
+    public bool EarnedStar() => (points / level) > 1000;
+}
+
+//other classes and enumerations can go here
